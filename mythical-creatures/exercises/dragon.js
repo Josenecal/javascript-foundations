925f6ff27c1fe2890af3dd37bbf114bac3bdb20a
Joseph Senecal <jasenecal2@gmail.com>
2024-05-13 17:37:56 -0600

    Dragon tests passing

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,40 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name,
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if(dragon.timesEaten >= 3) {
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+
+function findFireBreathers(dragons) {
+  var fireBreathers = [];
+  dragons.forEach((dragon) => {
+    if(dragon.temperment == 'aggressive') {
+      fireBreathers.push(dragon);
+    }
+  })
+  return fireBreathers;
+}
+
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat, 
+  findFireBreathers
 }
